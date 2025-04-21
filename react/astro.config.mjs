c2cc87393102047e38d3c80991e0201a2b311f04
zasowo <kamilkopischke@gmail.com>
2025-04-21 20:30:51 +0200

    test

--- react/astro.config.mjs
+++ react/astro.config.mjs
@@ -3,16 +3,25 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import node from '@astrojs/node';
+import react from '@astrojs/react'
+import path from 'path'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://podziemie.com.pl',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
   output: 'server', // enables server rendering
   adapter: node({
     mode: 'standalone',
   }),
   experimental: {
     session: true,
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
   },
 });
